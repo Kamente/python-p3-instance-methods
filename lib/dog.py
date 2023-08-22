c169d87169096971565ce35d6cbfd32d3d05b58d
Kamente <justinm48000@gmail.com>
2023-08-22 11:39:35 +0300

    done

--- lib/dog.py
+++ lib/dog.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 
-class Dog:
-    # Class body goes here
+class Dog: # class
+    def bark (self): # instance method(bark is now an instance of Dogs)
+        print("Woof!")
+    def sit (self): 
+        print("The dog is sitting.")
+        
+fido = Dog() # instance of Dog
+fido.bark()
+fido.sit()
 
-    #Instance method definition
-    pass
+snoop = Dog()
+snoop.bark()
